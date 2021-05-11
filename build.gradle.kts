c3999dc7d3789d6fe68116cb079a1cbdcb4ebc74
aQuu <marcin.lesinski.87@gmail.com>
2021-05-11 10:40:01 +0200

    forest-libbox

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `maven-publish`
 }
 group = "io.stud.forest"
-version = "1.2"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
@@ -15,52 +15,53 @@ kotlin {
         compilations.all {
             kotlinOptions.jvmTarget = "13"
         }
-    }
-    js {
-        browser {
-            testTask {
-                useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
+
+        js {
+            browser {
+                testTask {
+                    useKarma {
+                        useChromeHeadless()
+                        webpackConfig.cssSupport.enabled = true
+                    }
                 }
             }
         }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+        val hostOs = System.getProperty("os.name")
+        val isMingwX64 = hostOs.startsWith("Windows")
+        val nativeTarget = when {
+            hostOs == "Mac OS X" -> macosX64("native")
+            hostOs == "Linux" -> linuxX64("native")
+            isMingwX64 -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
 
-    
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+
+        sourceSets {
+            val commonMain by getting
+            val commonTest by getting {
+                dependencies {
+                    implementation(kotlin("test-common"))
+                    implementation(kotlin("test-annotations-common"))
+                }
             }
-        }
-        val jvmMain by getting{
-            dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
+            val jvmMain by getting {
+                dependencies {
+                    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
+                }
             }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit5"))
+            val jvmTest by getting {
+                dependencies {
+                    implementation(kotlin("test-junit5"))
+                }
             }
-        }
-        val jsMain by getting
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
+            val jsMain by getting
+            val jsTest by getting {
+                dependencies {
+                    implementation(kotlin("test-js"))
+                }
             }
+            val nativeMain by getting
+            val nativeTest by getting
         }
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
